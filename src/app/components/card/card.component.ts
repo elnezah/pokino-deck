6dd6e114c4bb4f984b4dcf1108ad435f105cd41c
elnezah <elnezah@gmail.com>
2022-03-21 23:56:34 +0100

    Started card imgs

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ToolboxService } from './../../services/toolbox.service';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Card } from 'src/app/shared/deck';
 
 @Component({
@@ -6,12 +13,27 @@ import { Card } from 'src/app/shared/deck';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-  private static readonly TAG = 'CardComponent';
+export class CardComponent implements OnInit, OnChanges {
+  private static readonly cTAG = 'CardComponent';
 
   @Input() card: Card;
 
-  public constructor() {}
+  public imgSrc: string;
+
+  public constructor(private tb: ToolboxService) {}
 
   public ngOnInit() {}
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    this.imgSrc = this.cardToImgFilePath(this.card);
+  }
+
+  private cardToImgFilePath(c: Card): string {
+    if (!c) {
+      return null;
+    }
+    const id = this.tb.addLeadingZeros(c.id, 2);
+    const n = this.tb.addLeadingZeros(c.cardNumber, 2);
+    return `assets/img/cards/${id}_${c.suit}_${n}.png`;
+  }
 }
